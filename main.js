6073479b72b7e21aa6680acda3c71b446f7bfb31
FuldauerP <pablofuldauer54@gmail.com>
2023-05-14 00:04:58 -0300

    Proyecto finalizado para cliente de DOTT BAIRES

---
+++ main.js
@@ -0,0 +1,7 @@
+/* app es el div
+<div id="iconos">
+      <a href="https://github.com/Emprinnos/bootStrapCompleto"><i class="bi bi-facebook"></i></a>
+      <a href="#"><i class="bi bi-twitter"></i></a>
+      <a href="#"><i class="bi bi-instagram"></i></a>
+    </div>
+    */
