2abe9a52f4d3b232caaa37fd34b66135b296baa4
FuldauerP <pablofuldauer54@gmail.com>
2023-05-14 01:37:35 -0300

    Creando clave SSH

--- main.js
+++
@@ -1,7 +0,0 @@
-/* app es el div
-<div id="iconos">
-      <a href="https://github.com/Emprinnos/bootStrapCompleto"><i class="bi bi-facebook"></i></a>
-      <a href="#"><i class="bi bi-twitter"></i></a>
-      <a href="#"><i class="bi bi-instagram"></i></a>
-    </div>
-    */
